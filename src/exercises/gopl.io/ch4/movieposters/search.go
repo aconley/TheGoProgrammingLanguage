5792af65e43ab8ff1a17affe1a70f6d4f6d679f6
Alexander Conley <alexanderconley@gmail.com>
2016-03-03 20:07:38 -0700

    Added preliminary movie poster json parser

---
+++ src/exercises/gopl.io/ch4/movieposters/search.go
@@ -0,0 +1,53 @@
+package movieposters
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+// getOMDBInfo queries the OMDB to get movie information
+func getOMDBInfo(name string) (*PosterSearchResult, error) {
+	q := url.QueryEscape(name)
+	resp, err := http.Get(QueryURL + "/?s=" + q)
+    if resp != nil {
+        defer resp.Body.Close()
+    }
+	if err != nil {
+		return nil, err
+	}
+	
+	// We must close resp.Body on all execution paths.
+	// (Chapter 5 presents 'defer', which makes this simpler.)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result PosterSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+// GetPosterURLs gets a slice of poster urls for movies
+func GetPosterURLs(name string) ([]string, error) {
+    info, err := getOMDBInfo(name);
+    if err != nil {
+        return nil, err
+    }
+    n := len(info.Items)
+    if n == 0 {
+        // Not an error, per se
+        return nil, nil
+    }
+    
+    urls := make([]string, 0, n)
+    for _, item := range(info.Items) {
+        if item.Type == "movie" && item.PosterURL != "N/A" {
+            urls = append(urls, item.PosterURL)
+        }
+    }
+    return urls, nil
+}
