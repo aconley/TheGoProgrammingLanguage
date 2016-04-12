53d51ffe8121bad98eebf9b4cdda1935f3c95f83
Alexander Conley <alexanderconley@gmail.com>
2016-04-11 20:07:59 -0600

    Handle special case of j = n-2 in lexicographic

--- src/combinatorics/permutations/lexicographic.go
+++ src/combinatorics/permutations/lexicographic.go
@@ -4,7 +4,6 @@ import "sort"
 
 // This generates integer permutations lexicographically
 //  using Algorithm L from Knuth vol 4, section 7.2.1.2
-// This is the basic (un-optimized) version
 
 // NextLexicographicPermutation modifies it's argument
 //  to the next lexicographic permutation, returning
@@ -14,10 +13,22 @@ import "sort"
 //  not considered different.  Thus, the permuatations
 //  of 113 are 113, 131, 311 only.
 func NextLexicographicPermutation(a sort.Interface) bool {
+
+  // This is slightly optimizied in the sense of problem
+  //  7.1.2.1.(1), but the full problem solution there
+  //  requires things that haven't been allowed in our interface
+  //  In fact, all we can do is handle the 'swap the last two'
+  //  special case.
 	n := a.Len()
-	if n == 0 {
+	if n < 2 {
 		return false
 	}
+
+  // Easiest case: j = n - 2, so we just swap the ends
+  if a.Less(n-2, n-1) {
+    a.Swap(n-2, n-1)
+    return true
+  }
 
 	j := n - 2
 	for j >= 0 && !a.Less(j, j+1) {
@@ -33,6 +44,7 @@ func NextLexicographicPermutation(a sort.Interface) bool {
 	//  visited all permutations beginning with a[0]..a[j].
 	//  So we will now have to make a[j] larger
 	// Figure out what to exchange it with.
+
 	l := n - 1
 	for !a.Less(j, l) {
 		l--
@@ -51,6 +63,7 @@ func NextLexicographicPermutation(a sort.Interface) bool {
 
 	return true
 }
+
 
 // Lexicographic visits all permutations of data in
 //  lexicographic order, calling visit on each
