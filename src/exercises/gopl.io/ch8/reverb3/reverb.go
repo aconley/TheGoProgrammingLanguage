19176eaa0c23d6a01671c8e4c2bedb9504f141ed
Alexander Conley <alexanderconley@gmail.com>
2016-04-16 21:58:36 -0600

    Exercise 8.8: Add timeout on reverb channel

--- src/exercises/gopl.io/ch8/reverb3/reverb.go
+++ src/exercises/gopl.io/ch8/reverb3/reverb.go
@@ -1,5 +1,5 @@
 // Reverb3 is a TCP server that simulates an echo.
-//  This version is modified as per exercise 8.4
+//  This version is modified as per exercise 8.4 and 8.8
 package main
 
 import (
@@ -15,21 +15,39 @@ import (
 func handleConn(c net.Conn) {
   var connTCP = c.(*net.TCPConn)
 
-  var wg sync.WaitGroup // Number of open echos per connection
-	input := bufio.NewScanner(c)
-	for input.Scan() {
-    log.Println("Starting new echo")
-    wg.Add(1)
+	inputChan := make(chan string)
 
-    go func(txt string, delay time.Duration) {
-      defer wg.Done()
-      fmt.Fprintln(connTCP, "\t", strings.ToUpper(txt))
-	    time.Sleep(delay)
-	    fmt.Fprintln(connTCP, "\t", txt)
-	    time.Sleep(delay)
-	    fmt.Fprintln(connTCP, "\t", strings.ToLower(txt))
-    }(input.Text(), 1 * time.Second)
+	// Puts text on inputChan
+	go func(connTCP *net.TCPConn) {
+		input := bufio.NewScanner(c)
+		for input.Scan() {
+			inputChan <- input.Text()
+		}
+	}(connTCP)
+
+  var wg sync.WaitGroup // Number of open echos per connection
+
+	timeOutTicker := time.NewTicker(10 * time.Second)
+
+	select {
+		case text := <- inputChan:
+			log.Println("Starting new echo")
+			wg.Add(1)
+			go func(txt string, delay time.Duration) {
+      	defer wg.Done()
+      	fmt.Fprintln(connTCP, "\t", strings.ToUpper(txt))
+	    	time.Sleep(delay)
+	    	fmt.Fprintln(connTCP, "\t", txt)
+	    	time.Sleep(delay)
+	    	fmt.Fprintln(connTCP, "\t", strings.ToLower(txt))
+    	}(text, 1 * time.Second)
+		case <-timeOutTicker.C:
+			log.Println("Heard nothing on channel; closing")
+			timeOutTicker.Stop()
+			connTCP.Close()
+			return
 	}
+	timeOutTicker.Stop()
 
   // Closer
   go func() {
